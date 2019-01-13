93407b774eb4dd625b8e8bd485d921c5d6bdfa3d
Marko Jagodic <maki.jagodic1@gmail.com>
2019-01-13 18:06:08 +0100

    added roleBasedAuthorization

---
+++ RoleBasedAuthorization/RoleBasedAuthorization/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoleBasedAuthorization.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Dashboard()
+        {
+            var current_user = (string)Session["username"];
+            var user_roles = MvcApplication.UserRoles;
+            var current_user_role = (string)user_roles[current_user];
+
+            if (current_user_role == "User")
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ RoleBasedAuthorization/RoleBasedAuthorization/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace RoleBasedAuthorization
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static string[] Roles;
+        public static Dictionary<string, object> UserRoles;
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Roles = new string[] { "Administrator", "User" };
+
+            UserRoles = new Dictionary<string, object>();
+            UserRoles.Add("admin", "Administrator");
+            UserRoles.Add("user", "User");
+        }
+    }
+}
